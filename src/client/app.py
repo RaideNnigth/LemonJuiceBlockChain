e0c5d2e572304e962a9df9bc54de60de8f84a274
Gustavo Pinzon Pereira <32282947+RaideNnigth@users.noreply.github.com>
2023-09-10 15:14:26 -0300

    Folder Structures

---
+++ src/client/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, url_for, request
+
+from ..blockchain.networkNodeClient import NetworkNodeClient, TransactionException
+from ..blockchain.initialize_blockchain import blockchain
+
+app = Flask(__name__)
+
+blockchain_base = blockchain()
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        task_content = request.form['content']
+        return render_template(url_for('index'))
+        
+    else: 
+        return render_template('home.html')
+
+
+@app.route("/transactions", methods=['POST'])
+def validate_transaction():
+    content = request.json
+    try:
+        node = NetworkNodeClient(blockchain_base)
+        node.receive(transaction=content["transaction"])
+        node.validade()
+        node.validate_funds()
+    except TransactionException as transaction_exception:
+        return f'{transaction_exception}', 400
+    return "Transaction success", 200
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
